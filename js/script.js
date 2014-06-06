4da465a48fe0f0fa309c6b8c40d435eea69b9c87
valmyrsjc <contatovalmyrmarques@gmail.com>
2014-06-06 18:34:34 -0300

    commit inicial do projeto HelpDesk

---
+++ js/script.js
@@ -0,0 +1,54 @@
+/* ================================================================================================*/
+
+/* funcão para o div conteudoChamados */
+
+function showChamados() {
+	if (document.getElementById('conteudoChamados').style.display == 'none') {
+		document.getElementById('conteudoChamados').style.display = 'block';
+	}
+	return false;
+}
+
+function hideChamados() {
+	if (document.getElementById('conteudoChamados').style.display == 'block') {
+		document.getElementById('conteudoChamados').style.display = 'none';
+	}
+	return false;
+}
+
+/* ================================================================================================*/
+
+/* funcão para o div conteudoUsuario */
+
+function showUsuarios() {
+	if (document.getElementById('conteudoUsuarios').style.display == 'none') {
+		document.getElementById('conteudoUsuarios').style.display = 'block';
+	}
+	return false;
+}
+
+function hideUsuarios() {
+	if (document.getElementById('conteudoUsuarios').style.display == 'block') {
+		document.getElementById('conteudoUsuarios').style.display = 'none';
+	}
+	return false;
+}
+
+/* ================================================================================================*/
+
+/* funcão para o div conteudoRelatorio */
+function showRelatorios() {
+	if (document.getElementById('conteudoRelatorios').style.display == 'none') {
+		document.getElementById('conteudoRelatorios').style.display = 'block';
+	}
+	return false;
+}
+
+function hideRelatorios() {
+	if (document.getElementById('conteudoRelatorios').style.display == 'block') {
+		document.getElementById('conteudoRelatorios').style.display = 'none';
+	}
+	return false;
+}
+/* ================================================================================================*/
+
